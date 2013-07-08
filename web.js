068f6ff68a01d2c3f33fb42185a944efee80406d
John Smith <example@stanford.edu>
2013-07-08 10:15:35 +0000

    modifying index.html for HW02-2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send((fs.readFileSync('index.html')).toString("utf-8"));
+    response.send((fs.readFileSync('index.html'))1.toString("utf-8"));
 
 });
 
